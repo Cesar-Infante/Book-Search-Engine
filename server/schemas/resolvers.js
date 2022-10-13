0658682eb27f27c3affe8c567cd83f719d6b791a
cesar-infante <cesar261110@gmail.com>
2022-10-12 22:12:09 -0700

    defined the necessary Query and Mutation types

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,4 @@
-const { Book, User } = require('../models')
+const { AuthenticationError } = require('apollo-server-express')
+const { User } = require('../models');
+const { signToken } = require('../utils/auth')
 
-const resolvers = {
-    
-}
